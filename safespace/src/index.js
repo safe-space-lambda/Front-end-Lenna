66efd6edfd07f6212256506612a2b05329cc2467
Lenna <lenna@lennamau.com>
2019-02-04 14:38:36 -0700

    sign up form

--- safespace/src/index.js
+++ safespace/src/index.js
@@ -2,8 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from './store/reducers'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk, logger))
+// );
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+//   <Provider store={store}>
+//     <Router>
+      <App />,
+//     </Router>
+//   </Provider>,
+  rootElement
+);
 
